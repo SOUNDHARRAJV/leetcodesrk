2e7890ebc35dae66ce1b4b51640d6228ac102f90
SOUNDHAR RAJ V <soundharrajvellingiri@gmail.com>
2026-03-11 12:15:08 +0530

    Time: 5 ms (79.20%) | Memory: 45.5 MB (99.06%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int y=x;
+        int rev=0;
+        while(x>0){
+            rev=rev*10+(x%10);
+            x/=10;
+        }
+        return (rev==y);
+    }
+}
